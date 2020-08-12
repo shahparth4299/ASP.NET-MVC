8b3bb1e37f16e6f58d92e8a7325bf956d7487350
Parth Shah <parth4299.shah@gmail.com>
2020-08-12 08:52:30 +0000

    Delegation Code

---
+++ DelegateDemo1/DelegateDemo1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace DelegateDemo1
+{
+    class Program
+    {
+        delegate void pointer();
+        static pointer point;
+        static void Main(string[] args)
+        {
+            point += Demo1;
+            point += Demo2;
+            point.Invoke();
+            Console.Read();
+        }
+        static void Demo1()
+        {
+            Console.WriteLine("Function 1");
+        }
+        static void Demo2()
+        {
+            Console.WriteLine("Function 2");
+        }
+    }
+}
