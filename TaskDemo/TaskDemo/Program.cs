be081681383fb99ee6f62ab7b40b4e4303096bb2
Parth Shah <parth4299.shah@gmail.com>
2020-08-12 09:09:54 +0000

    Other Codes

---
+++ TaskDemo/TaskDemo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+namespace TaskDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Demo1();
+            Demo2();
+            Console.WriteLine("End of Main");
+            Console.Read();
+        }
+        static void Demo1()
+        {
+            Console.WriteLine("Inside the Demo1");
+        }
+        static void Demo2()
+        {
+            Console.WriteLine("Inside the Demo2");
+        }
+    }
+}
---
+++ ThreadBasic/ThreadBasic/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadBasic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread t1 = new Thread(new ThreadStart(Demo));
+            Thread t2 = new Thread(new ThreadStart(Demo2));
+            t1.Start();
+            t2.Start();
+            Console.WriteLine("Completed");
+            Console.Read();
+        }
+        static void Demo()
+        {
+            Thread.Sleep(5000);
+            Console.WriteLine("Inside 1st Function");
+        }
+        static void Demo2()
+        {
+            Console.WriteLine("Inside 2nd Function");
+        }
+    }
+}
